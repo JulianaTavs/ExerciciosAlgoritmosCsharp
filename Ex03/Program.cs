86ba03338c8c7f5ffb7395a00224e992afcb4f14
Juliana <julianatavaresdasilva@gmail.com>
2025-01-16 18:23:26 -0300

    adicionando esse exercício

---
+++ Ex03/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Qual o seu nome?");
+String? nome = Console.ReadLine();
+Console.WriteLine("Qual o seu salário?");
+String? salary = Console.ReadLine();
+float salario = float.Parse(salary!);
+
+Console.WriteLine($"A(O) funcionária(o) {nome} tem um salário de R$ {salario} próximo mês.");
