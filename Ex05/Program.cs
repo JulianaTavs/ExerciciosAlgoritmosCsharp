82be4ba6b15f5f77b7b32c3db5587bb8c2984f34
Juliana <julianatavaresdasilva@gmail.com>
2025-01-20 18:51:58 -0300

    exercicio adicionado

---
+++ Ex05/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Digite a primeira nota: ");
+float nota1 = LerNumero();
+Console.WriteLine("Digite a segunda nota: ");
+float nota2 = LerNumero();
+float media = (nota1 + nota2) / 2;
+Console.WriteLine($"A média entre {nota1} e {nota2} é igual a {media}");
+
+static float LerNumero()
+{
+    float numero;
+    while (true)
+    {
+        string entrada = Console.ReadLine();
+        if (float.TryParse(entrada, out numero))  // Verifica se a entrada é um número válido
+        {
+            return numero;  // Retorna a entrada se for um número válido
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida! Digite uma nota válida:");
+        }
+    }
+}
