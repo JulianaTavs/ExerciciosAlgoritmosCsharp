989ee99c3c05961b440c93350a0c02b788ee82ac
Juliana <julianatavaresdasilva@gmail.com>
2025-01-20 12:19:43 -0300

    update

---
+++ Ex04/Program.cs
@@ -0,0 +1,57 @@
+﻿
+Menu();
+
+static void Menu()
+{
+
+    Soma();
+    Console.Clear();
+    Continuar();
+}
+static void Soma()
+{
+
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro número: ");
+    int num1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Digite o segundo número: ");
+    int num2 = int.Parse(Console.ReadLine()!);
+    int soma = num1 + num2;
+
+    Console.WriteLine($"O resultado da soma dos dois números é: {soma}");
+    Thread.Sleep(1000);
+}
+static void Continuar()
+{
+    int opcao;
+    do
+    {
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("Você gostaria de usar novamente a calculadora de somar?");
+        Console.WriteLine(" 1 - SIM");
+        Console.WriteLine(" 2 - NÃO. Encerrar programa.");
+        if (int.TryParse(Console.ReadLine(), out opcao))
+        {
+            switch (opcao)
+            {
+                case 1:
+                    Soma();
+                    break;
+                case 2:
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Essa opção não existe, tente novamente.");
+                    Thread.Sleep(1000);
+                    Continuar();
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida! Digite 1 ou 2.");
+            Thread.Sleep(1000);
+        }
+    } while (true);
+}
