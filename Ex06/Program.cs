5bac0393fe9123f797ba257e5d4418bfbe8a30a2
Juliana <julianatavaresdasilva@gmail.com>
2025-01-20 19:18:28 -0300

    exercicio add

---
+++ Ex06/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.WriteLine("Digite um número inteiro: ");
+int numero = LerNumero();
+int antecessor;
+int sucessor;
+
+antecessor = numero - 1;
+sucessor = numero + 1;
+
+Console.WriteLine($"O antecessor de {numero} é {antecessor}.");
+Console.WriteLine($"O sucessor de {numero} é {sucessor}.");
+
+static int LerNumero()
+{
+    int numero;
+
+
+    while (true)
+    {
+        string entrada = Console.ReadLine()!;
+        if (int.TryParse(entrada, out numero))
+        {
+            return numero;
+        }
+        else
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Digite um número válido: ");
+        }
+    }
+}
