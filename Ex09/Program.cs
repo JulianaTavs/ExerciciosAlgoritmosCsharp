4371d10539801eeb4dbfa8a9bc87db4755c7f6c2
Juliana <julianatavaresdasilva@gmail.com>
2025-01-21 18:46:57 -0300

    exercicio add

---
+++ Ex09/Program.cs
@@ -0,0 +1,31 @@
+﻿CalculadoraDeConversao();
+static void CalculadoraDeConversao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o valor em R$ que você deseja converter para dólar: ");
+    float valor = LerNumero();
+
+    float ValorEmDolar = valor / 6.03f;
+    Console.WriteLine($"A conversão de R${valor} em dólar fica U${ValorEmDolar:F2}");
+}
+static float LerNumero()
+{
+    float numero;
+
+    while (true)
+    {
+        string data = Console.ReadLine()!;
+
+        if (float.TryParse(data, out numero))
+        {
+            return numero;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Entrada inválida! Por favor, digite um número válido.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Digite o valor que você deseja converter para dólar:");
+        }
+    }
+}
