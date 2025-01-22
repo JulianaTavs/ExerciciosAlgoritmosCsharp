efec57315943cbe762e12e87b6177999804282d4
Juliana <julianatavaresdasilva@gmail.com>
2025-01-22 13:49:55 -0300

    exercicio add

---
+++ Ex11/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("Vamos calcular o valor de delta. Para isso: ");
+Console.WriteLine("Digite o valor de a: ");
+float a = LerNumero();
+Console.WriteLine("Digite o valor de b: ");
+float b = LerNumero();
+Console.WriteLine("Digite o valor de c: ");
+float c = LerNumero();
+
+float delta = (b * b) - (4 * a * c);
+Console.WriteLine($"Delta vale: {delta}");
+
+static float LerNumero()
+{
+    float numero;
+
+    while (true)
+    {
+        string data = Console.ReadLine();
+        if (float.TryParse(data, out numero))
+        {
+            return numero;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Valor inválido! Por favor digite o valor novamente:");
+            Thread.Sleep(2000);
+        }
+    }
+}
