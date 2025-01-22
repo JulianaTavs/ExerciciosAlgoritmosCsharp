da9be502643a0b6d84dfaa6fc830458c043b87ed
Juliana <julianatavaresdasilva@gmail.com>
2025-01-22 14:28:51 -0300

    exercicio add

---
+++ Ex12/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Digite o preço do produto para calcular o seu desconto: ");
+float preco = LerNumero();
+float desconto = (float)(preco * 0.05);
+float valorPromocional = preco - desconto;
+
+Console.WriteLine($"PREÇO PROMOCIONAL: R$ {valorPromocional}");
+
+static float LerNumero()
+{
+    float numero;
+
+    while (true)
+    {
+        string data = Console.ReadLine();
+        if (float.TryParse(data, out numero))
+        {
+            return numero;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido! Digite o valor novamente.");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Digite o preço do produto para calcular o seu desconto:");
+        }
+    }
+}
