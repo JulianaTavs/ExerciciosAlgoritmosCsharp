bf101996ee45086d54e7c8eceec54ea4fed430ba
Juliana <julianatavaresdasilva@gmail.com>
2025-01-23 12:15:28 -0300

    exercicio add

---
+++ Ex14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+Console.Clear();
+Console.WriteLine("Digite a quantidade de Km percorridos pelo carro alugado: ");
+float kmPercorridos = LerNumero();
+Console.WriteLine("Digite a quantidade de dias pelos quais ele foi alugado: ");
+int qtdeDias = 0;
+
+while (true)
+{
+    string data = Console.ReadLine()!;
+    if (int.TryParse(data, out qtdeDias))
+    {
+        float totalAPagar = (90 * qtdeDias) + (float)(0.20 * kmPercorridos);
+        Console.WriteLine($"TOTAL A PAGAR: R$ {totalAPagar:F2}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Valor inválido! ");
+        Thread.Sleep(2000);
+        Console.Clear();
+        Console.WriteLine("Favor digite novamente a quantidade de dias pelos quais ele foi alugado: ");
+        Thread.Sleep(2000);
+    }
+}
+
+static float LerNumero()
+{
+    float numero;
+    while (true)
+    {
+        string data = Console.ReadLine()!;
+        if (float.TryParse(data, out numero))
+        {
+            return numero;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido! ");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Favor digite novamente a quantidade de Km percorridos pelo carro alugado: ");
+            Thread.Sleep(2000);
+        }
+    }
+}
