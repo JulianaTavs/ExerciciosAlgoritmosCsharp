975870cd02adc4855319523894e63306806b701d
Juliana <julianatavaresdasilva@gmail.com>
2025-02-03 15:06:25 -0300

    exercicio add

---
+++ Ex25/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Digite o valor do primeiro segmento de reta: ");
+double a = LerSegmento();
+Console.WriteLine("Digite o valor do segundo segmento de reta: ");
+double b = LerSegmento();
+Console.WriteLine("Digite o valor do terceiro segmento de reta: ");
+double c = LerSegmento();
+
+if ((a + b) > c && (a + c) > b && (b + c) > a)
+{
+    System.Console.WriteLine("É possível formar um triângulo com esses três segmentos de reta.");
+}
+else
+{
+    System.Console.WriteLine("Não é possível formar um triângulo com esses três segmentos de reta.");
+}
+
+
+static double LerSegmento()
+{
+    while (true)
+    {
+        string entrada = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(entrada) && double.TryParse(entrada, out double valor) && valor > 0)
+        {
+            return valor;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido. Favor digitar um número positivo: ");
+        }
+    }
+}
