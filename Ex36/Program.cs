d454e5da46756741c0651f05533343424a9f90ae
Juliana <julianatavaresdasilva@gmail.com>
2025-02-16 14:30:49 -0300

    exercicio add

---
+++ Ex36/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Quantas horas de atividade física você teve no mês?");
+decimal horasMes = LerValor(1, 744);
+int totalPontos = CalcularPontos(horasMes);
+
+decimal valorGanhoNoTotal = totalPontos * 0.05m;
+
+System.Console.WriteLine("----------------------------------------");
+System.Console.WriteLine($"TOTAL DE PONTOS: {totalPontos}");
+System.Console.WriteLine($"TOTAL EM DINHEIRO GANHO: {valorGanhoNoTotal:C}");
+System.Console.WriteLine("----------------------------------------");
+
+static decimal LerValor(int min, int max)
+{
+    while (true)
+    {
+        if (decimal.TryParse(Console.ReadLine(), out decimal valor) && valor >= min && valor <= max)
+        {
+            return valor;
+        }
+        else
+        {
+            System.Console.WriteLine("Valor inválido. Favor digitar um valor válido entre " +
+            $"{min} e {max}(que é o total de horas em um mês): ");
+        }
+    }
+}
+
+static int CalcularPontos(decimal horas)
+{
+    if (horas <= 10)
+    {
+        return (int)horas * 2;
+    }
+    else if (horas <= 20)
+    {
+        return (int)horas * 5;
+    }
+    else
+    {
+        return (int)horas * 10;
+    }
+}
