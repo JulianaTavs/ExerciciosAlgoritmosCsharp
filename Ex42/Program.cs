da20a95cfd51d801f952f915192eb7af0dff761f
Juliana <julianatavaresdasilva@gmail.com>
2025-02-17 19:29:18 -0300

    exercicio add

---
+++ Ex42/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Digite um número inteiro e positivo qualquer: ");
+int numero = LerNumero();
+
+int i = 1;
+
+Console.WriteLine("---------------------------");
+while (i <= numero)
+{
+    Console.Write(i + " ");
+    i++;
+}
+Console.WriteLine("Acabou!");
+Console.WriteLine("---------------------------");
+
+static int LerNumero()
+{
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int valor) && valor > 0)
+        {
+            return valor;
+        }
+        else
+        {
+            System.Console.WriteLine("Entrada inválida. Favor digitar um número inteiro positivo: ");
+        }
+    }
+}
