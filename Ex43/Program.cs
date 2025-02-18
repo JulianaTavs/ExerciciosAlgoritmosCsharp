01a1967f1548e138c3a2417c8e4b797356b599c4
Juliana <julianatavaresdasilva@gmail.com>
2025-02-18 12:16:09 -0300

    exercicio add

---
+++ Ex43/Program.cs
@@ -0,0 +1,15 @@
+﻿int i = 30;
+
+while (i > 0)
+{
+    if (i % 4 == 0)
+    {
+        Console.Write("[" + i + "]" + " ");
+    }
+    else
+    {
+        Console.Write(i + " ");
+    }
+    i--;
+}
+
