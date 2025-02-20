a63309e5217fd45a734450cb15c6d292a8eb5887
Juliana <julianatavaresdasilva@gmail.com>
2025-02-20 16:45:55 -0300

    exercicio add

---
+++ Ex49/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Entre com seis números inteiros:");
+int pares = 0;
+int impares = 0;
+
+LerInteiros();
+
+Console.WriteLine($"Qtde de números pares: {pares}");
+Console.WriteLine($"Qtde de números ímpares: {impares}");
+
+void LerInteiros()
+{
+    int contador = 1;
+    int numero;
+
+    while (contador <= 6)
+    {
+        if (int.TryParse(Console.ReadLine(), out numero))
+        {
+            if (numero % 2 == 0)
+            {
+                pares++;
+            }
+            else
+            {
+                impares++;
+            }
+            contador++;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido. Só são aceitos números inteiros. " +
+            "Favor digitar um número inteiro para prosseguirmos:");
+        }
+
+    }
+}
