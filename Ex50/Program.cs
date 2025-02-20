f16abb7b4bc40e932a4a4aafba45b767d87f5425
Juliana <julianatavaresdasilva@gmail.com>
2025-02-20 17:59:35 -0300

    exercicio add

---
+++ Ex50/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("----------------------------------------------------------------");
+Console.WriteLine("Vamos sortear 20 números: ");
+
+int contador = 1;
+int numero;
+Random numeroAleatorio = new Random();
+int numerosAcimaDeCinco = 0;
+int numerosDivisiveisPorTres = 0;
+
+Console.WriteLine(" ");
+Console.Write("Números sorteados: ");
+
+while (contador <= 20)
+{
+    numero = numeroAleatorio.Next(0, 11);
+    Console.Write(numero + " ");
+
+    if (numero % 3 == 0)
+    {
+        numerosDivisiveisPorTres++;
+    }
+
+    if (numero > 5)
+    {
+        numerosAcimaDeCinco++;
+    }
+    contador++;
+}
+
+Console.WriteLine("");
+Console.WriteLine($"Qtde de números acima de 5: {numerosAcimaDeCinco} ");
+Console.WriteLine($"Qtde de números divisíveis por 3: {numerosDivisiveisPorTres} ");
+Console.WriteLine("----------------------------------------------------------------");
