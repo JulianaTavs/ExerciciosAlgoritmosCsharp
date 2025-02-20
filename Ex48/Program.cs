1faf291d3e2aa977f809554f989c195bf333dbde
Juliana <julianatavaresdasilva@gmail.com>
2025-02-20 16:02:01 -0300

    exercicio add

---
+++ Ex48/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite sete números inteiros: ");
+
+int contador = 1;
+int numero;
+int soma = 0;
+
+while (contador <= 7)
+{
+    numero = int.Parse(Console.ReadLine());
+    soma += numero;
+    contador++;
+}
+
+Console.WriteLine($"Somatório: {soma}");
