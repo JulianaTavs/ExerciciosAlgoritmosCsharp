af3a033dd9ce44ac16626180b64aa5752187f0ca
Juliana <julianatavaresdasilva@gmail.com>
2025-03-04 10:37:17 -0300

    exercicio add

---
+++ Ex55/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Welcome to the Guess The Number game!");
+Console.WriteLine("You'll have 4 tries to guess the number correctly.\n");
+
+Random random = new Random();
+int aleatorio = random.Next(1, 11);
+int guess = 0;
+int i = 1;
+
+Console.WriteLine("Adivinhe o número! É entre 1 e 10:");
+while (i <= 4)
+{
+    Console.WriteLine($"Tentativa {i}. Qual número você vai escolher nessa tentativa: ");
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out guess) && guess >= 1 && guess <= 10)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido. Só é aceito um número entre 1 e 10. Tente novamente:");
+        }
+    }
+
+    if (guess == aleatorio)
+    {
+        Console.WriteLine($"Você acertou! Seu número escolhido foi: {guess}");
+        break;
+    }
+    i++;
+}
+Console.WriteLine();
+
+if (guess != aleatorio)
+{
+    Console.WriteLine("Não foi dessa vez :/");
+}
+
+Console.WriteLine($"O número aleatório era: {aleatorio}");
+Console.WriteLine("--------------------------------------------------");
