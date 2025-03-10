2c1ceffeef014c8487f51d7685f01215e7a221f4
Juliana <julianatavaresdasilva@gmail.com>
2025-03-10 14:34:06 -0300

    exercicio added

---
+++ Ex58/Aluno.cs
@@ -0,0 +1,29 @@
+namespace Ex58
+{
+    class Aluno
+    {
+        public int Idade;
+        public Aluno()
+        {
+            Idade = LerIdade();
+        }
+        public int LerIdade()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int numero) && numero > 0)
+                {
+                    return numero;
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido. Digite novamente a idade(ou 999 para terminar): ");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Idade}";
+        }
+    }
+}
---
+++ Ex58/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using Ex58;
+
+List<Aluno> alunos = new List<Aluno>();
+
+int contador = 0;
+int somaIdades = 0;
+
+while (true)
+{
+    Console.WriteLine($"Entre a idade do aluno {contador + 1}(ou 999 para terminar):");
+    Aluno aluno = new Aluno();
+
+    if (aluno.Idade == 999)
+        break;
+
+    alunos.Add(aluno);
+    contador++;
+    somaIdades += aluno.Idade;
+}
+
+Console.WriteLine("-------------------------------------------------");
+
+double mediaIdades;
+if (contador > 0)
+{
+    mediaIdades = (double)somaIdades / contador;
+    Console.WriteLine($"Média das idades: {mediaIdades.ToString("F2", CultureInfo.InvariantCulture)}");
+    Console.WriteLine($"Qtde de alunos: {contador}");
+}
+else
+{
+    Console.WriteLine("Nenhum aluno cadastrado!");
+}
+
+int i = 1;
+
+alunos.ForEach(item =>
+{
+    Console.WriteLine($"Idade do aluno {i}: {item}");
+    i++;
+});
+
+
