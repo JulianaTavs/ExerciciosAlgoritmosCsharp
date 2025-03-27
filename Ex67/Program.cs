ac576a3fdb212e18e738a20bfdf19312464a75a6
Juliana <julianatavaresdasilva@gmail.com>
2025-03-27 14:09:32 -0300

    exercicio added

---
+++ Ex67/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Digite um número inteiro: ");
+int numero = LerInteiro();
+
+Console.Write("Contagem: ");
+for (int i = 0; i <= numero; i++)
+{
+    if (i == numero)
+    {
+        Console.Write(i);
+    }
+    else
+    {
+        Console.Write($"{i}, ");
+    }
+}
+
+Console.Write(", FIM!");
+
+int LerInteiro()
+{
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int saida))
+        {
+            return saida;
+        }
+        else
+        {
+            Console.WriteLine("valor inválido. Favor digitar um número inteiro: ");
+        }
+    }
+}
