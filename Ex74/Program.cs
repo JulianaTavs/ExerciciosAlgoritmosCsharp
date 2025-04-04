cd96d0f361f091ad8adebb6be356bbcc15a181c0
Juliana <julianatavaresdasilva@gmail.com>
2025-04-04 13:23:44 -0300

    exercicio added

---
+++ Ex74/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] vetor = new int[10];
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        vetor[i] = 5;
+    }
+    else
+    {
+        vetor[i] = 3;
+    }
+}
+
+Console.WriteLine(String.Join(" ", vetor));
