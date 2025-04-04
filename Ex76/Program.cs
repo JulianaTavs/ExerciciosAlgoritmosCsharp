9ef7103c879863391b04fa50fd4dc7b2b46fe7fb
Juliana <julianatavaresdasilva@gmail.com>
2025-04-04 13:58:59 -0300

    exercicio added

---
+++ Ex76/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] vetor = new int[7];
+Random random = new Random();
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    int aleatorio = random.Next(0, 1001);
+    vetor[i] = aleatorio;
+}
+
+Console.WriteLine(String.Join(" ", vetor));
