4205c9157dff85dc8d1e9bd75b4d8ab933b3553d
Juliana <julianatavaresdasilva@gmail.com>
2025-04-04 11:47:40 -0300

    exercicio added

---
+++ Ex72/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] vetor = new int[10];
+int soma = 0;
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    soma += 5;
+    vetor[i] = soma;
+}
+
+foreach (var item in vetor)
+{
+    Console.Write(item + " ");
+}
+
+// Ou usar isso para a impressão do vetor: Console.WriteLine(string.Join(" ", vetor));
