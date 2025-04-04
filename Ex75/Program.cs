6adff734f492a24e024f12fbb76be40bf920a8a8
Juliana <julianatavaresdasilva@gmail.com>
2025-04-04 13:39:08 -0300

    exercicio added

---
+++ Ex75/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] vetor = new int[15];
+
+vetor[0] = 1;
+vetor[1] = 1;
+
+for (int i = 2; i < vetor.Length; i++)
+{
+    vetor[i] = vetor[i - 1] + vetor[i - 2];
+}
+
+Console.WriteLine(String.Join(" ", vetor));
