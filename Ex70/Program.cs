bcea90e510b012b1aef6731b9db2b5c5dc25909a
Juliana <julianatavaresdasilva@gmail.com>
2025-04-04 10:37:41 -0300

    exercicio added

---
+++ Ex70/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] sequenciaFibonacci = new int[10];
+
+sequenciaFibonacci[0] = 1;
+sequenciaFibonacci[1] = 1;
+
+for (int i = 2; i < sequenciaFibonacci.Length; i++)
+{
+    sequenciaFibonacci[i] = sequenciaFibonacci[i - 1] + sequenciaFibonacci[i - 2];
+}
+
+foreach (var item in sequenciaFibonacci)
+{
+    Console.Write(item + " ");
+}
