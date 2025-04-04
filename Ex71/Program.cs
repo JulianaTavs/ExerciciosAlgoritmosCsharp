36d4130950391454b0cbab0cd79a055986dd9c45
Juliana <julianatavaresdasilva@gmail.com>
2025-04-04 10:51:14 -0300

    exercicio added

---
+++ Ex71/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] vetorNumerico = new int[8];
+
+for (int i = 0; i < vetorNumerico.Length; i++)
+{
+    vetorNumerico[i] = 999;
+}
+
+foreach (var item in vetorNumerico)
+{
+    Console.Write(item + " ");
+}
