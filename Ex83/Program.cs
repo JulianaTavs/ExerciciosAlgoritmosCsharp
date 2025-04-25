74f6cdd65ef3e9f5b55092429668cfa010183655
Juliana <julianatavaresdasilva@gmail.com>
2025-04-25 11:26:39 -0300

    exercicio added

---
+++ Ex83/Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int[] numerosAleatorios = new int[20];
+
+for (int i = 0; i < numerosAleatorios.Length; i++)
+{
+    numerosAleatorios[i] = random.Next(0, 100);
+}
+
+foreach (var item in numerosAleatorios)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine("Números em ordem crescente: ");
+
+Array.Sort(numerosAleatorios);
+
+foreach (var item in numerosAleatorios)
+{
+    Console.Write(item + " ");
+}
+
+
