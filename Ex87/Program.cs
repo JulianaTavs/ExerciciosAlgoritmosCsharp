8d0d561d8c60bbc96951f83f936afa0af7042fae
Juliana <julianatavaresdasilva@gmail.com>
2025-04-28 10:33:02 -0300

    exercicio added

---
+++ Ex87/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+void Gerador(string mensagem)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine("+-------=======------+");
+    Console.WriteLine(mensagem);
+    Console.WriteLine("+-------=======------+");
+    Console.ResetColor();
+}
+
+Gerador("Aprendendo C# 😍");
+Console.WriteLine("Digite qualquer tecla para sair...");
+Console.ReadKey();
