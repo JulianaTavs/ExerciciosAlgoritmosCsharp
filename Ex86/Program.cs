e7d409e15941f74166cb48b5639c9e15f2fe0493
Juliana <julianatavaresdasilva@gmail.com>
2025-04-28 10:02:59 -0300

    exercicio added

---
+++ Ex86/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+void Gerador()
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("+-------=======------+");
+    Console.WriteLine("Olá, Mundo!");
+    Console.WriteLine("+-------=======------+");
+
+    Console.ResetColor();
+
+}
+
+Gerador();
+
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine();
+Console.Write("Pressione qualquer tecla para sair...");
+Console.ResetColor();
+Console.ReadKey();
+
