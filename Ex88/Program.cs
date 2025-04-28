cbc7548727ffdc52646ff822fa436eec38ec7f11
Juliana <julianatavaresdasilva@gmail.com>
2025-04-28 12:03:20 -0300

    exercicio added

---
+++ Ex88/Program.cs
@@ -0,0 +1,13 @@
+﻿void Gerador(string mensagem, int qtsVezesMostrarMensagem)
+{
+    Console.WriteLine("+-------=======------+");
+
+    for (int i = 0; i < qtsVezesMostrarMensagem; i++)
+    {
+        Console.WriteLine(mensagem);
+    }
+
+    Console.WriteLine("+-------=======------+");
+}
+
+Gerador("Aprendendo C#", 4);
