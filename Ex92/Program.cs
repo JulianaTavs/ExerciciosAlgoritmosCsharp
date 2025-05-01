8feab5caf55ef3eb8bd7e4e6428a05dc2b9c5276
Juliana <julianatavaresdasilva@gmail.com>
2025-05-01 09:30:16 -0300

    exercicio added

---
+++ Ex92/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Digite um número inteiro: ");
+int num = ObterInteiroValido();
+
+ParOuImpar(num);
+
+int ObterInteiroValido()
+{
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int saida))
+        {
+            return saida;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido. Favor digitar um número inteiro: ");
+        }
+    }
+}
+
+static void ParOuImpar(int numero)
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($"O número {numero} é par.");
+    }
+    else
+    {
+        Console.WriteLine($"O número {numero} é ímpar.");
+    }
+}
